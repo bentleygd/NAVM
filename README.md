7fb8ff99b0cb8391f02158df7226381bcb837151
Gabriel Bentley <gabriel.bentley.us@gmail.com>
2020-11-16 13:52:18 -0800

    Documentation update.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # NAVM - Nessus Auotmated Vulnerability Management
+
+The purpose of the Nessus Automated Vulnerability Management project is to provide vulnerability management automation to organizations that do not have the budget for Tenable's products that have built-in automation and integration features.  The project has the following features:
+
+* Scan broker.
+* Scan self service with authentication.
+* User authentication via LDAP.
+* A client script that can be utilized to interface with the scan broker.
+* Intergration with JIRA for ticketing.
+
+<h2>Third Party Python Package Pre-requisites</h2>
+
+* flask
+* flask-mongoengine
+* mongoengine
+* ldap3
+* requests
+* mod-wsgi
+
+<h2>Server Requirements</h2>
+
+* MongoDB
+* Apache
+
+<h2>Documentation</h2>
+
+Documenation is located [here](https://github.com/bentleygd/NAVM/DOCs.md).
--- common/navm_lib.py
+++ common/navm_lib.py
@@ -22,13 +22,23 @@ def nessus_session_login(url, uname, passwd):
     than five seconds to respond."""
     log = getLogger(__name__)
     params = {'username': uname, 'password': passwd}
+    # Connecting and authenticating to Nessus.
     try:
         response = request('POST', url, params=params, timeout=5)
         response.raise_for_status()
+    # If we get an HTTP error, log it.
     except HTTPError:
-        log.exception('HTTP error returned by the Nessus server.')
+        log.exception(
+            'HTTP error returned by the Nessus server.  Unable to retrieve' +
+            'session token.'
+        )
+        exit(1)
+    # If the connection times out, log it.
     except Timeout:
-        log.exception('Connection to Nessus server timed out.')
+        log.exception(
+            'Connection to Nessus server timed out.  Unable to obtain a' +
+            ' session cookie.')
+        exit(1)
     token = response.json()[0]['token']
     return token
 
@@ -40,8 +50,8 @@ def nessus_session_logoff(url, token):
     server.
 
     Inputs:
-    url - The URL of the session server.  Example:
-    https://server.domain:8834/session/destroy
+    url - The session URL of the Nessus scanner.  Example:
+    https://nessus_scanner.domain.tld:8834/session
     token - The session token returned by the nessus_login function.
 
     Outputs:
@@ -53,14 +63,25 @@ def nessus_session_logoff(url, token):
     Timeout - An exception occurs when the Nessus server takes more
     than five seconds to respond."""
     log = getLogger(__name__)
+    # The value of the X-Cookie header must be the session token
+    # previously retrieved by the nessus_session_login function.
     headers = {'X-Cookie': 'token=' + token}
+    # Connect to Nessus server and destroy session.
     try:
         response = request('DELETE', url, headers=headers, timeout=5)
         response.raise_for_status()
+    # If we get an HTTP error, log it.
     except HTTPError:
-        log.exception('HTTP error returned by the Nessus server.')
+        log.exception(
+            'HTTP error returned by the Nessus server.  Unable to ' +
+            'delete session token.'
+        )
+    # Log timeouts - this shouldn't occur as a connection to the Nessus
+    # server was already established.
     except Timeout:
-        log.exception('Connection to Nessus server timed out.')
+        log.exception(
+            'Connection to Nessus server timed out.  Unable to delete' +
+            ' session.')
     response_code = response.status_code
     return response_code
 
@@ -82,16 +103,28 @@ def get_nessus_key(url, token):
     Timeout - An exception occurs when the Nessus server takes more
     than five seconds to respond."""
     log = getLogger(__name__)
+    # The value of the X-Cookie header must be the session token
+    # previously retrieved by the nessus_session_login function.
     headers = {'X-Cookie': 'token=' + token}
     try:
         response = request('PUT', url, headers=headers, timeout=5)
         response.raise_for_status()
+    # If we get an HTTP error, log it.
     except HTTPError:
-        log.exception('HTTP error returned by the Nessus server.')
+        log.exception(
+            'HTTP error returned by the Nessus server.  Unable to ' +
+            'retrieve an API key.'
+        )
+        exit(1)
+    # If the connection times out, log it.
     except Timeout:
-        log.exception('Connection to Nessus server timed out.')
+        log.exception(
+            'Connection to Nessus server timed out.  Unable to ' +
+            'retrieve API key.'
+        )
+        exit(1)
     api_key = {
-        'accessKey': response.json()[0]['accessKey'],
-        'secretKey': response.json()[0]['secretKey']
+        'access_key': response.json()[0]['accessKey'],
+        'secret_key': response.json()[0]['secretKey']
     }
     return api_key
